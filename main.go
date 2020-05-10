73da7b8cc73ee9ff1fc6b26c9ae7187b13547be4
James Andrews <jgandrews@gmail.com>
2020-05-10 16:08:40 +0100

    Working prototype

--- main.go
+++ main.go
@@ -1,16 +1,49 @@
 package main
 
 import (
-	"io"
+	"fmt"
 	"log"
+	"net"
+	"strings"
 
 	"github.com/gliderlabs/ssh"
+	"github.com/oschwald/geoip2-golang"
 )
 
-func main() {
-	ssh.Handle(func(s ssh.Session) {
-		io.WriteString(s, s.User()+"@"+s.LocalAddr().String()+"'s Password: ")
-	})
+func passwordHandler(ctx ssh.Context, password string) bool {
 
-	log.Fatal(ssh.ListenAndServe(":2222", nil))
+	// Get Location
+	db, err := geoip2.Open("GeoLite2-City.mmdb")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+	// If you are using strings that may be invalid, check that ip is not nil
+	ip := net.ParseIP("216.58.204.238")
+	record, err := db.City(ip)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	lat := fmt.Sprintf("%f", record.Location.Latitude)
+	long := fmt.Sprintf("%f", record.Location.Longitude)
+
+	ipAddr := strings.Split(ctx.RemoteAddr().String(), ":")[0]
+
+	jsonOutput := "{user: \"" + ctx.User() + "\", password: \"" + password + "\", ip_address: \"" + ipAddr + "\",city: \"" + record.City.Names["en"] + "\", region: \"" + record.Subdivisions[0].Names["en"] + "\", country: \"" + record.Country.Names["en"] + "\",latitude: \"" + lat + "\",longitude: \"" + long + "\"}"
+
+	log.Println(jsonOutput)
+
+	return false
+}
+
+func main() {
+	sPort := "2222"
+	s := &ssh.Server{
+		Addr: ":" + sPort,
+		//Handler:         sessionHandler,
+		PasswordHandler: passwordHandler,
+	}
+	log.Printf("Starting ssh-honeypot on port %s\n", sPort)
+	log.Fatal(s.ListenAndServe())
 }
